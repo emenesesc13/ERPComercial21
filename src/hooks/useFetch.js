f3d83679a1790cef807920f21a59e5ae00bd7897
Jhostin Osorio <66564971+JhostinOsorio@users.noreply.github.com>
2021-04-18 22:04:24 -0500

    Adding hook useFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import axiosIns from '@/libs/axios'
+
+const useFetch = async (url = '/', params = {}, method = 'GET', expectedStatus = 200) => {
+  const info = {
+    data: null,
+    error: null,
+  }
+  let response
+  try {
+    if (method.toUpperCase().trim()==='POST') {
+      response = await axiosIns.post(url, params)
+    } else {
+      response = await axiosIns.get(url)
+    }
+    if (response.status !== expectedStatus) throw new Error(response)
+    info.data = response.data
+  } catch (error) {
+    info.error = error
+  }
+  return info
+}
+
+export default useFetch
