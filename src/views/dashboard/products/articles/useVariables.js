19f8882c70d70e188f0e057126476e4f9b0b2fa8
Jhostin Osorio <66564971+JhostinOsorio@users.noreply.github.com>
2021-04-22 09:45:10 -0500

    Changing article variables

--- src/views/dashboard/products/articles/useVariables.js
+++ src/views/dashboard/products/articles/useVariables.js
@@ -8,55 +8,53 @@ const useVariables = () => {
   })
 
   const initialArticle = {
-    id: 0,
+    _id: 85,
+    nombre: '',
+    idGrupoUnidad: 15,
+    idUnidadInventario: 11,
+    idUnidadVenta: 11,
+    idTipoProducto: 0,
     sku: '',
-    productType: null,
-    articleName: '',
-    stock: false,
-    service: false,
-    unitGroup: null,
-    inventoryUnit: null,
-    unitSale: null,
-    purchasePrice: null,
-    salePrice: null,
-    minimumSalePrice: null,
-    minimumStock: null,
-    maximumStock: null,
-    features: [],
+    precioCompra: null,
+    precioVenta: null,
+    precioMinimoVenta: null,
+    stockMinimo: null,
+    stockMaximo: null,
+    flgStock: false,
+    flgServicio: false,
+    accion: 0,
+    idUsuario: null,
+    loading: false,
   }
 
-  // const initialArticle = {
-  //   _id: 0,
-  //   sku: '',
-  //   nombre: '',
-  //   idTipoProducto: null,
-  //   idGrupoUnidad: null,
-  //   idUnidadInventario: null,
-  //   idUnidadVenta: null,
-  //   precioCompra: null,
-  //   precioVenta: null,
-  //   precioMinimoVenta: null,
-  //   stockMinimo: null,
-  //   stockMaxim: null,
-  //   flgStock: false,
-  //   flgServicio: false,
-  //   accion: null,
-  //   idUsuario: null,
-  //   features: [],
-  // }
+  const initialArticleFeature = {
+    _id: 0,
+    nombre: '',
+    idCaracteristica: 0,
+    accion: 0,
+    idUsuario: null,
+    loading: false,
+  }
 
+  
   const article = ref({ ...initialArticle })
-
+  
   const resetArticle = () => {
     article.value = { ...initialArticle }
   }
 
+  const articleFeature = ref({ ...initialArticleFeature })
+  
+  const resetArticleFeature = () => {
+    articleFeature.value = { ...initialArticleFeature }
+  }
+  
   const initialStateCombo = {
     disabled: true,
     loading: false,
     data: [],
   }
-
+  
   const combos = ref({
     productTypes: { ...initialStateCombo },
     unitGroup: { ...initialStateCombo },
@@ -66,26 +64,12 @@ const useVariables = () => {
     valuesByFeature: { ...initialStateCombo },
   })
 
-  const defaultSelectedUnitGroup = {
-    unitGroup: 15,
-    inventoryUnit: 11,
-    unitSale: 11,
-  }
-
   const resetCombos = (nameCombos = []) => {
     nameCombos.forEach(name => {
       combos.value[name] = { ...initialStateCombo }
     })
-    // combos.value = {
-    //   productTypes: { ...initialStateCombo },
-    //   unitGroup: { ...initialStateCombo },
-    //   inventoryUnit: { ...initialStateCombo },
-    //   unitSale: { ...initialStateCombo },
-    //   features: { ...initialStateCombo },
-    //   valuesByFeature: { ...initialStateCombo },
-    // }
   }
-
+  
   const featureSelected = ref(null)
   const valueSelected = ref(null)
 
@@ -121,15 +105,59 @@ const useVariables = () => {
     articles,
     article,
     resetArticle,
+    articleFeature,
+    resetArticleFeature,
     initialStateCombo,
     combos,
+    resetCombos,
     featureSelected,
     valueSelected,
-    defaultSelectedUnitGroup,
-    resetCombos,
     serverParams,
     optionsColumnsFilter,
   }
 }
 
 export default useVariables
+
+// const initialArticle = {
+  //   id: 0,
+  //   sku: '',
+  //   productType: null,
+  //   articleName: '',
+  //   stock: false,
+  //   service: false,
+  //   unitGroup: null,
+  //   inventoryUnit: null,
+  //   unitSale: null,
+  //   purchasePrice: null,
+  //   salePrice: null,
+  //   minimumSalePrice: null,
+  //   minimumStock: null,
+  //   maximumStock: null,
+  //   features: [],
+  // }
+  
+  // const initialArticle = {
+    //   _id: 0,
+    //   sku: '',
+    //   nombre: '',
+    //   idTipoProducto: null,
+    //   idGrupoUnidad: null,
+    //   idUnidadInventario: null,
+//   idUnidadVenta: null,
+//   precioCompra: null,
+//   precioVenta: null,
+//   precioMinimoVenta: null,
+//   stockMinimo: null,
+//   stockMaxim: null,
+//   flgStock: false,
+//   flgServicio: false,
+//   accion: null,
+//   idUsuario: null,
+//   features: [],
+// }
+// const defaultSelectedUnitGroup = {
+//   unitGroup: 15,
+//   inventoryUnit: 11,
+//   unitSale: 11,
+// }
