2bcb33f67a24a6fec1b0b0b46b296a011e73ef7a
emenesesc13 <82726519+emenesesc13@users.noreply.github.com>
2021-05-17 14:53:16 -0500

    Modules tab finished
    - List Module
    - Seach Module
    - Save Module
    - Update Module
    - Delete Module
    - Activate / Desactivate Module

--- src/views/dashboard/security/modules/Module.vue
+++ src/views/dashboard/security/modules/Module.vue
@@ -1,22 +1,74 @@
 <template>
   <div>
+    <modal-module />
+    <modal-search-module />
     <b-card>
-      <h1>Modulos</h1>
+      <table-module />
     </b-card>
   </div>
 </template>
 
 <script>
+import { inject, provide, onMounted } from '@vue/composition-api'
 import { BCard } from 'bootstrap-vue'
+import useFetch from '@/hooks/useFetch'
+import useVariables from './useVariables'
+import TableModule from './TableModule.vue'
+import ModalModule from './ModalModule.vue'
+import ModalSearchModule from './ModalSearchModule.vue'
 
 export default {
   name: 'Module',
   components: {
     BCard,
+    TableModule,
+    ModalModule,
+    ModalSearchModule,
+  },
+  setup() {
+    const {
+      modules, moduleOne, resetModule, optionsColumnsFilter, serverParams, combos,
+    } = useVariables()
+
+    const messageToast = inject('messageToast')
+    const loadComboBoxes = inject('loadComboBoxes')
+
+    const loadModules = async () => {
+      modules.value.loading = true
+      const { columnFilters, page, perPage } = serverParams.value
+      const { field, value } = columnFilters
+      let url = `/modulos/?_id=0&tabla=modulos&pinicio=${page}&pfin=${perPage}`
+      if (field) url += `&campofiltro=${field}&filtro=${value}`
+      const { data, error } = await useFetch(url)
+      if (error) {
+        messageToast('danger', 'Error', 'Error al momento de cargar los modulos')
+      } else if (data) {
+        modules.value.data = data
+        modules.value.totalRecords = 0
+        if (data?.length) {
+          if (data[0]?.numberRow) modules.value.totalRecords = data[0].numberRow
+        }
+      }
+      modules.value.loading = false
+    }
+
+    onMounted(() => {
+      loadModules()
+      loadComboBoxes(combos.value, ['moduleCmb'], '/combo/modulos/0', 'Error al momento de cargar los grupo unidad')
+    })
+
+    provide('modules', modules)
+    provide('loadModules', loadModules)
+    provide('moduleOne', moduleOne)
+    provide('resetModule', resetModule)
+    provide('optionsColumnsFilter', optionsColumnsFilter)
+    provide('serverParams', serverParams)
+    provide('combos', combos)
   },
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  @import '@core/scss/vue/libs/vue-select.scss';
+  @import '@core/scss/vue/libs/vue-sweetalert.scss';
 </style>
